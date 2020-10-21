05cde4d1ff8aafb4563d77f6c423e0e94af0834a
TravelFiend <mikegrace274@gmail.com>
2020-10-21 17:27:26 -0400

    class-ified the csvfilereader

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,22 @@
-import fs from 'fs';
+import { CsvFileReader } from './CsvFileReader';
 
-const matches = fs.readFileSync('football.csv', {
-  encoding: 'utf-8'
-}).split('\n').map((row: string): string[] => row.split(','));
+const reader = new CsvFileReader('football.csv');
+reader.read();
 
 let manUWins = 0;
 
-for (let match of matches) {
-  if (match[1] === 'Man United' && match[5] === 'H') {
+enum MatchResult {
+  HomeWin = 'H',
+  AwayWin = 'A',
+  Draw = 'D'
+}
+
+for (let match of reader.data) {
+  if (match[1] === 'Man United' && match[5] === MatchResult.HomeWin) {
     manUWins++;
-  } else if (match[2] === 'Man United' && match[5] === 'A') {
+  } else if (match[2] === 'Man United' && match[5] === MatchResult.AwayWin) {
     manUWins++;
   };
 }
 
 console.log(`Manchester United won ${manUWins} games`);
-
