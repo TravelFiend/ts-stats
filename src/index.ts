809a7a0c27deebf324040e448283545f9bd4d40c
TravelFiend <mikegrace274@gmail.com>
2020-10-21 15:48:12 -0400

    "bad" code in place

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,18 @@
-console.log('America');
+import fs from 'fs';
+
+const matches = fs.readFileSync('football.csv', {
+  encoding: 'utf-8'
+}).split('\n').map((row: string): string[] => row.split(','));
+
+let manUWins = 0;
+
+for (let match of matches) {
+  if (match[1] === 'Man United' && match[5] === 'H') {
+    manUWins++;
+  } else if (match[2] === 'Man United' && match[5] === 'A') {
+    manUWins++;
+  };
+}
+
+console.log(`Manchester United won ${manUWins} games`);
+
