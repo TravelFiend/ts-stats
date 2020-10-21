6d66025e2e62d7fd9fc3e4072d443203b509b013
TravelFiend <mikegrace274@gmail.com>
2020-10-21 19:56:02 -0400

    initial refactor

--- build/CsvFileReader.js
+++ build/CsvFileReader.js
@@ -13,7 +13,7 @@ var CsvFileReader = /** @class */ (function () {
     CsvFileReader.prototype.read = function () {
         this.data = fs_1.default.readFileSync(this.filename, {
             encoding: 'utf-8'
-        }).split('\n').map(function (row) { return row.split(','); });
+        }).split('\n').map(function (row) { return row.split(','); }).map(this.mapRow);
     };
     return CsvFileReader;
 }());
--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,10 @@
-import { CsvFileReader } from './CsvFileReader';
+import { MatchReader } from './MatchReader';
+import { MatchResult } from './MatchResult';
 
-const reader = new CsvFileReader('football.csv');
+const reader = new MatchReader('football.csv');
 reader.read();
 
 let manUWins = 0;
-
-enum MatchResult {
-  HomeWin = 'H',
-  AwayWin = 'A',
-  Draw = 'D'
-}
 
 for (let match of reader.data) {
   if (match[1] === 'Man United' && match[5] === MatchResult.HomeWin) {
